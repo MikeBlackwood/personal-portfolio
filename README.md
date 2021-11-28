7ec7e37f6b1ddcffffa08bc5eec673f65c1575cb
Michael Budei <48001535+upsilonbridge@users.noreply.github.com>
2021-11-27 21:42:34 -0800

    added submit check

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # personal-portfolio
 One page personal portfolio.
-In order to run project you can used 
+In order to run project you can use live server or open index.html file.
+The site is deployed on Github pages
--- contact.js
+++ contact.js
@@ -5,13 +5,37 @@ let message = document.getElementById("message");
 let submitBtn = document.getElementById("submit-email");
 let restBtn = document.getElementById("reset-form");
 let form = document.getElementById("email-form");
-
+let submitBTn = document.getElementById("submit-form");
 function resetEmailForm()
 {
-    console.log(firstName);
+    firstName.style.borderColor = "#212529"
+    lastName.style.borderColor = "#212529"
+    email.style.borderColor = "#212529"
+    message.style.borderColor = "#212529"
     firstName.value = "";
     lastName.value = "";
     email.value = "";
     message.value = "";
+}
+
+
+function submitForm()
+{
+    if(firstName.value == "")
+    {
+        firstName.style.borderColor = "red"
+    }
+    if(lastName.value == "")
+    {
+        lastName.style.borderColor = "red"
+    }
+    if(email.value == "" || !email.value.includes("@"))
+    {
+        email.style.borderColor = "red"
+    }
+    if(message.value == "")
+    {
+        message.style.borderColor = "red"
+    }
 }
 
